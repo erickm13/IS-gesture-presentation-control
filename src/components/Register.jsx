a0a56952a199e74c735467d8ee02a4a4e86fd018
Carlos Alvarez <23004004@galileo.edu>
2025-11-20 19:46:08 -0600

    Integración de firebase y priimer usuario creado satisfactoriamente

--- src/components/Register.jsx
+++ src/components/Register.jsx
@@ -1,17 +1,45 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { FaGoogle } from "react-icons/fa";
-import "./../styles/Login.css"; // ← ¡Usamos el MISMO CSS!
+import "./../styles/Register.css";
+import { auth, db } from "../firebase/config"; 
+import { doc, setDoc } from "firebase/firestore";
 
 export default function Register() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [name, setName] = useState("");
+  const [lastname, setLastname] = useState("");
   const navigate = useNavigate();
 
-  const handleSubmit = (e) => {
+  const createUser = async (e) => {
     e.preventDefault();
-    alert(`Usuario creado: ${email}`);
-    navigate("/login");
+
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+
+      const user = userCredential.user;
+
+      await setDoc(doc(db, "users", user.uid), {
+        uid: user.uid,
+        email,
+        name,
+        lastname,
+        role: "user",
+      });
+
+      alert("Usuario creado con éxito");
+      navigate("/login");
+
+    } catch (error) {
+      console.error("Error al registrar:", error);
+      alert(error.message);
+    }
   };
 
   const googleRegister = () => {
@@ -20,10 +48,24 @@ export default function Register() {
 
   return (
     <div className="center">
-      <div className="login-container"> 
+      <div className="login-container">
         <h2>Crear Usuario</h2>
 
-        <form onSubmit={handleSubmit}>
+        <form onSubmit={createUser}>
+          <input
+            type="text"
+            placeholder="Nombre"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+
+          <input
+            type="text"
+            placeholder="Apellido"
+            value={lastname}
+            onChange={(e) => setLastname(e.target.value)}
+          />
+
           <input
             type="email"
             placeholder="Correo electrónico"
